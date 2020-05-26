70da6554363370e0ebe4da1615591d935415b73c
Oran C <Oranbusiness@gmail.com>
2020-05-26 13:06:55 -0700

    [add] small app change for test

--- client_config.py
+++ client_config.py
@@ -1,5 +1,5 @@
 class ClientConfig(object):
-    PUBLIC_KEY = 'None'
+    PUBLIC_KEY = 'B7Dqz+/lLGNvVJwFb8LgVt+R5VnnuettyXatkPctGsQ'
     APP_NAME = 'update-demo'
     COMPANY_NAME = 'wisehackermonkey'
     HTTP_TIMEOUT = 30
--- main.py
+++ main.py
@@ -4,11 +4,16 @@
 # 20200526
 
 import tkinter as tk
-
+import __init__
 window = tk.Tk()
 
-greeting = tk.Label(text="Demo updating app")
 
+
+greeting = tk.Label(text="Demo updating app:")
 greeting.pack()
 
+version_float = tk.StringVar()
+version_float.set(__init__.__version__)
+version = tk.Label(window, textvariable=version_float)
+version.pack()
 window.mainloop()
