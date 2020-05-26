045cb6bf132e20879f93fcdd2f5c8d0eb7f901c6
Oran C <Oranbusiness@gmail.com>
2020-05-26 12:39:31 -0700

    [init] files

---
+++ client_config.py
@@ -0,0 +1,7 @@
+class ClientConfig(object):
+    PUBLIC_KEY = 'None'
+    APP_NAME = 'update-demo'
+    COMPANY_NAME = 'wisehackermonkey'
+    HTTP_TIMEOUT = 30
+    MAX_DOWNLOAD_RETRIES = 3
+    UPDATE_URLS = ['64.227.84.118']
---
+++ main.py
@@ -0,0 +1,14 @@
+# by oran collins
+# github.com/wisehackermonkey
+# oranbusiness@gmail.com
+# 20200526
+
+import tkinter as tk
+
+window = tk.Tk()
+
+greeting = tk.Label(text="Demo updating app")
+
+greeting.pack()
+
+window.mainloop()
